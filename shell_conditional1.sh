15d222997ac10c6b0ac5f3a719c22eb2af713191
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2022-02-21 17:26:48 +0530

    [ command

---
+++ shell_conditional1.sh
@@ -0,0 +1,15 @@
+read X
+read Y
+read Z
+
+if [ $X == $Y -a $Y == $Z ]
+then 
+    echo "EQUILATERAL"
+elif [ $X == $Y -o $Y == $Z -o $Z == $X ]
+then
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi
+
+# [ is the test command and evaluates subsequent expression and returns exit code (if 0 , then is executed )
