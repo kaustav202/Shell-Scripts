0d93a1e60086feb31cca4509bd97ba2c3aafc7e6
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2022-02-21 13:35:25 +0530

    added new methods

--- shell_arith.sh
+++ shell_arith.sh
@@ -5,3 +5,17 @@ echo "$((X+Y))"
 echo "$((X-Y))"
 echo "$((X*Y))"
 echo "$((X/Y))"
+
+echo "\n"
+
+echo "$X + $Y" | bc
+echo "$X - $Y" | bc
+echo "$X * $Y" | bc
+echo "$X / $Y" | bc
+
+echo "\n"
+
+echo "`expr $X + $Y`"
+echo "`expr $X - $Y`"
+echo "`expr $X '*' $Y`"
+echo "`expr $X / $Y`"
